40012924df9c4351b6d03fe1ed9bd90154be4f78
Shalitha Suranga <shalithasuranga@gmail.com>
2018-09-15 03:47:49 +0530

    Add EndScreen

---
+++ src/components/EndScreen/EndScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './EndScreen.css';
+
+class EndScreen extends Component {
+  render() {
+    return (
+      <div className="GameScreen">
+        End Screen!
+      </div>
+    );
+  }
+}
+
+export default EndScreen;
