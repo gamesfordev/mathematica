62118a59316400c077a94b36e3d74899a437e926
Asce4s <asce4s@gmail.com>
2018-09-15 21:57:36 +0530

    leaderbrd ui

--- src/components/EndScreen/EndScreen.js
+++ src/components/EndScreen/EndScreen.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './EndScreen.css';
 import Grid from '@material-ui/core/Grid';
+import Paper from "@material-ui/core/es/Paper/Paper";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -12,16 +20,17 @@ class EndScreen extends Component {
   constructor() {
     super();
     this.state = {
-      winners: [],
-      username: window.localStorage.getItem('username')
+        players: [],
+      username: window.localStorage.getItem('username'),
+        rowsPerPage:10,
+        page:0
     };
     let dbRef = fire.orderByChild('score');
     dbRef.on('value', snapshot => {
-      const winners = Object.values(snapshot.val())
-        .sort((a, b) => b.score - a.score)
-        .slice(0, 3);
+      const players = Object.values(snapshot.val())
+        .sort((a, b) => b.score - a.score);
       this.setState({
-        winners: winners
+        players: players
       });
     });
     this.share = this.share.bind(this);
@@ -36,55 +45,82 @@ class EndScreen extends Component {
     );
   }
 
+    handleChangePage = (event, page) => {
+        this.setState({ page });
+    };
+
+    handleChangeRowsPerPage = event => {
+        this.setState({ rowsPerPage: event.target.value });
+    };
+
   render() {
     return (
       <div className="EndScreen">
-        <h1 className="title"> Game Title </h1>
-        <Grid container spacing={2} justify="center">
-          <Grid item md={8}>
-            <Grid item md={12} className="main-box">
-              <h1>Winner message</h1>
-            </Grid>
-            <Grid item md={12} className="main-box">
-              <Grid item md={12}>
-                <h1>Highe scores</h1>
+        <h1 className="title"> Mathematica logo here</h1>
+
+          <Grid container spacing={24} className="container">
+
+              <Grid item xs={9} className="left-screen">
+                  <Paper id="leaderbrd" >
+
+                      <Table >
+                          <TableHead>
+                              <TableRow>
+                                  <TableCell>Player</TableCell>
+                                  <TableCell numeric>Score</TableCell>
+                              </TableRow>
+                          </TableHead>
+                          <TableBody>
+                              {this.state.players.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map(row => {
+                                  return (
+                                      <TableRow key={row.id}>
+
+                                          <TableCell>{row.user}</TableCell>
+                                          <TableCell numeric>{row.score}</TableCell>
+
+                                      </TableRow>
+                                  );
+                              })}
+                          </TableBody>
+                          <TableFooter>
+                              <TableRow>
+                                  <TablePagination
+                                      colSpan={2}
+                                      count={this.state.players.length}
+                                      rowsPerPage={this.state.rowsPerPage}
+                                      page={this.state.page}
+                                      onChangePage={this.handleChangePage}
+                                      onChangeRowsPerPage={this.handleChangeRowsPerPage}
+
+                                  />
+                              </TableRow>
+                          </TableFooter>
+                      </Table>
+
+                  </Paper>
               </Grid>
-              <Grid item md={12}>
-                {this.state.winners.map(i => (
-                  <Card className="winner-item">
-                    <CardContent>
-                      <h2>
-                        {' '}
-                        {i.user} - {i.score}
-                      </h2>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small" color="primary">
-                        View
+              <Grid item xs={3} className="right-screen">
+                  <Paper  style={{padding:"20px"}}>
+                      <div id="congratz">
+                          Congratz !!!!
+
+                      </div>
+
+                      <Button variant="contained" color="primary" onClick={this.share}>
+                      Share
                       </Button>
-                    </CardActions>
-                  </Card>
-                ))}
+
+
+                      <Link
+                          to={this.state.username ? `/game/${this.state.username}` : `/`}
+                      >
+                          <Button variant="contained" color="primary">
+                              {this.state.username ? 'Play Again' : 'Play'}
+                          </Button>
+                      </Link>
+                  </Paper>
               </Grid>
-            </Grid>
           </Grid>
-          <Grid item md={4} className="main-box" justify="center">
-            <Grid item md={12} className="button">
-              <Button variant="contained" color="primary" onClick={this.share}>
-                Share
-              </Button>
-            </Grid>
-            <Grid item md={12} className="button">
-              <Link
-                to={this.state.username ? `/game/${this.state.username}` : `/`}
-              >
-                <Button variant="contained" color="primary">
-                  {this.state.username ? 'Play Again' : 'Play'}
-                </Button>
-              </Link>
-            </Grid>
-          </Grid>
-        </Grid>
       </div>
     );
   }
