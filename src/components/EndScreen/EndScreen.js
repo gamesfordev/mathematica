3f106532c441b22f81130107bf6abe4c29db4c77
dilantha111 <dilantha.prasanjith111@gmail.com>
2018-09-15 12:31:22 +0530

    leader board basic layout

--- src/components/EndScreen/EndScreen.js
+++ src/components/EndScreen/EndScreen.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
 import './EndScreen.css';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 class EndScreen extends Component {
   render() {
     return (
-      <div className="GameScreen">
-        End Screen!
+      <div className="EndScreen">
+        <h1 className="title"> Game Title </h1>
+        <Grid container spacing={2} justify="center">
+          <Grid item md={8}>
+            <Grid item md={12} className="main-box">
+              <h1>Winner message</h1>
+            </Grid>
+            <Grid item md={12} className="main-box">
+              <Grid item md={12}>
+                <h1>Highe scores</h1>
+              </Grid>
+              <Grid item md={12}>
+                {[1, 2, 3].map(i => (
+                  <Card className="winner-item">
+                    <CardContent>
+                      <h2> Name {i}</h2>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                    </CardActions>
+                  </Card>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item md={4} className="main-box" justify="center">
+            <Grid item md={12} className="button">
+              <Button variant="contained" color="primary">
+                Share
+              </Button>
+            </Grid>
+            <Grid item md={12} className="button">
+              <Button variant="contained" color="primary">
+                Play Again
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     );
   }
--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './GameScreen.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class GameScreen extends Component {
   render() {
     return (
       <div className="GameScreen">
-        Hello React!
+        <div className="master">
+          <div className="login_box">
+            <div className="logo">
+              <img src="" />
+            </div>
+            <div className="name">
+              <form className="loginForm"> 
+              <TextField
+                id="name"
+                label="Your Name"
+                value=""
+                margin="normal"
+                className="user_name"
+              />
+              <Button variant="contained" color="primary" className="">
+                Login
+              </Button>
+
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
