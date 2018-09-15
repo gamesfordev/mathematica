535859d518aed52c4efa7c257a0a590c98f2b3fa
Asce4s <asce4s@gmail.com>
2018-09-15 13:42:41 +0530

    gamescreen ui basic

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
 import './GameScreen.css';
 import GameArea from './GameArea/GameArea';
+import Grid from "@material-ui/core/es/Grid/Grid";
+import Paper from "@material-ui/core/es/Paper/Paper";
+import AppBar from "@material-ui/core/es/AppBar/AppBar";
+import Toolbar from "@material-ui/core/es/Toolbar/Toolbar";
+import Typography from "@material-ui/core/es/Typography/Typography";
 
 class GameScreen extends Component {
+
+
   render() {
     return (
       <div className="GameScreen">
-        <div className="row">
-            <div className="left-screen">
-                <div className="game-ui">
-                    <GameArea></GameArea>
-                </div>
-            </div>
-            <div className="right-screen">
-                <div className="score-area">
-                    dd
-                </div>
-                <div className="chances">
-                    ff
-                </div>
-            </div>
-        </div>
+          <AppBar position="static" color="default">
+              <Toolbar>
+                  <Typography variant="title" color="inherit">
+                      Mathamatica
+                  </Typography>
+              </Toolbar>
+          </AppBar>
+
+          <Grid container spacing={24} className="container">
+
+              <Grid item xs={9} className="left-screen">
+                  <Paper className="game">
+                      <GameArea></GameArea>
+                  </Paper>
+              </Grid>
+              <Grid item xs={3} className="right-screen">
+                  <Paper className="score">
+                      <h4>Score</h4>
+
+                  </Paper>
+                  <Paper className="chances">
+                      <h4>Chances</h4>
+                  </Paper>
+              </Grid>
+
+          </Grid>
+
+
       </div>
     );
   }
