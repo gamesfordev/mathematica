5bfd40769e047684b245a61bbb0a8eff2a0ae47b
Asce4s <asce4s@gmail.com>
2018-09-15 14:10:54 +0530

    ui height fix

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -9,6 +9,28 @@ import Typography from "@material-ui/core/es/Typography/Typography";
 
 class GameScreen extends Component {
 
+    constructor(){
+        super();
+        this.state={
+            gameScreenHeight: 0
+        }
+
+    }
+
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    updateDimensions = ()=>{
+        console.log("ddddddd")
+        const gameScreenHeight =( document.querySelectorAll('.game')[0].clientHeight/2)-22;
+        this.setState({gameScreenHeight: gameScreenHeight})
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
 
   render() {
     return (
@@ -24,17 +46,22 @@ class GameScreen extends Component {
           <Grid container spacing={24} className="container">
 
               <Grid item xs={9} className="left-screen">
-                  <Paper className="game">
+                  <Paper className="game" >
                       <GameArea></GameArea>
                   </Paper>
               </Grid>
               <Grid item xs={3} className="right-screen">
-                  <Paper className="score">
+                  <Paper className="score" style={{height:this.state.gameScreenHeight}}>
                       <h4>Score</h4>
-
+gfdfgdfgdf
+                      fdgfdg
+                      dfgdfg
                   </Paper>
-                  <Paper className="chances">
+                  <Paper className="chances" style={{height:this.state.gameScreenHeight}}>
                       <h4>Chances</h4>
+                      fgdfgd
+                      dfgdfg
+                      dfg
                   </Paper>
               </Grid>
 
